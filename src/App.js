c45fa15d83a66c35d9c9423d4cdb84e4bbec83a0
tizianatortone <tiziana_tortone@hotmail.com>
2021-03-29 14:47:38 +0200

    Search engine added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
       <header className="App-header">
-
+<main>
+  <Dictionary />
+</main>
       </header>
+<footer className="App-footer">
+  Coded by Tiziana Tortone
+</footer>
       </div>
     </div>
   );
