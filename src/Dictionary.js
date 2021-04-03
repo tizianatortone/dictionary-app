90415f5ce1d3e00763bb4876572243e4aef8e6ea
tizianatortone <tiziana_tortone@hotmail.com>
2021-04-03 19:17:05 +0200

    images added w/ another API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,33 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary () {
     const [keyword, setKeyword] = useState("");
     const [results, setResults] = useState(null);
+    const [photos, setPhotos] = useState (null);
 
 function handleResponse (response){
 setResults(response.data[0]);
 }
 
+function handlePexelsResponse (response) {
+    console.log(response);
+    setPhotos(response.data.photos);
+}
     function search(event){
         event.preventDefault();
    
     let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
-    }
+
+    const pexelsApiKey = "563492ad6f91700001000001ed51df52bb9540e6a1b18ffed49adcac";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers : headers }).then(handlePexelsResponse);
+  }
 
     function handleKeywordChange(event){
         setKeyword(event.target.value);
@@ -32,6 +43,7 @@ setResults(response.data[0]);
 
           </section>
           <Results results={results}/>
+          <Photos photos={photos} />
         </div>
     )
 }
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -6,7 +6,8 @@ export default function Synonyms (props) {
     return (
         <span className="Synonyms">
         <p><em>Synonyms: </em></p>
-        <ul>
+    
+            <ul>
             
             {props.synonyms.map(function(synonym, index) {
                 return (
